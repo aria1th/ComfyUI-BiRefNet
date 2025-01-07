583643242db90101b1007c0fcc7b1a239cf0e0c5
AngelBottomless <aria1th@naver.com>
2025-01-08 02:36:41 +0900

    auto install

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 import os
 import folder_paths
+from install import initialization
+initialization()
 from .BiRefNet_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 NODE_CLASS_MAPPINGS = {**NODE_CLASS_MAPPINGS}
